1070049d9099643678ee210b37ea250e328e60f0
HasniSenthil <sathiyahasni456@gmail.com>
2026-09-14 09:21:07 +0530

    Complete Week 2 class  problems

---
+++ src/main/java/oop/class_problems/CSVStudentRecordParser.java
@@ -0,0 +1,17 @@
+package oop.class_problems;
+public class CSVStudentRecordParser {
+    static void parseStudentRecord(String csvLine) {
+        String[] fields = csvLine.split(",");
+        if (fields.length != 3) {
+            System.out.println("Invalid Record");
+            return;
+        }
+        System.out.println("Name: " + fields[0]+ " | Roll No: " + fields[1]+ " | Dept: " + fields[2]);
+    }
+    public static void main(String[] args) {
+        String csvLine = "Ananya Verma,RA2211003010123,CSE";
+        String csvLine1 = "Ananya Verma,CSE";
+        parseStudentRecord(csvLine);
+        parseStudentRecord(csvLine1);
+    }
+}
